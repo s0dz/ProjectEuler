4403686254e10682a0a41086df0ab2c0109d26e2
s0dz <garrett.mctear@gmail.com>
2017-03-23 14:21:02 -0600

    Added unit test project

---
+++ ProjectEuler.UnitTests/ProgramTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace ProjectEuler.UnitTests
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [Test]
+        public void Problem3()
+        {
+            var number = 13195;
+
+            ProjectEuler.Program.Problem3(number);
+        }
+    }
+}
